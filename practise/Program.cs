e61e0cd71aa94e63ef7fa150de901e2cfde6a753
Prajakta Diware <Prajakta Diware@DESKTOP-9V2G23J>
2017-08-28 14:59:49 +0530

    Add project files.

---
+++ practise/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practise
+{
+    class Program
+    {
+        public class employee
+        {
+            public void display(int a, string b)
+            {
+                Console.WriteLine("Employee id is:" + " " + a);
+                Console.WriteLine("Employee Name is:" + " " + b);
+                Console.ReadKey();
+            }
+
+        }
+        public class trainee
+        {
+            public void show(ref string str)
+            {
+                Console.WriteLine("designation is:" + " " + str);
+                Console.ReadKey();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string m = "prajakta";
+            employee emp = new employee();
+            emp.display(1, m);
+
+            string n = "trainee";
+            trainee emp1 = new trainee();
+            emp1.show(ref n);
+
+
+        }
+    }
+}
---
+++ practise2/Program1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace practise2
+{
+
+    class StaticVar
+    {
+        public static int num;
+        public void count()
+        {
+            num++;
+        }
+        public static int getNum()
+        {
+            return num;
+        }
+    }
+    class StaticTester
+    {
+        static void Main(string[] args)
+        {
+            StaticVar s = new StaticVar();
+            s.count();
+            s.count();
+            s.count();
+            Console.WriteLine("Variable num: {0}", StaVar.getNum());
+            Console.ReadKey();
+        }
+    }
+}
+  /*public class Exercise47
+    {
+        public static void Main()
+        {
+            int[] nums = { 1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1 };
+            Console.WriteLine("\nArray1: [{0}]", string.Join(", ", nums));
+            var sum = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            }
+            Console.WriteLine("Sum: " + sum);
+            Console.ReadKey();
+        }
+    }
+}*/
+
+/*
+namespace practise2
+{
+   public class Exercise2
+    {
+         public static void Main()
+        {
+            Console.Write("Input a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input the desired width: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            int height = width;
+            for (int row = 0; row < height; row++)
+            {
+                for (int column = 0; column < width; column++)
+                {
+                    Console.Write(num);
+                }
+
+                Console.WriteLine();
+                width--;
+            }
+            Console.ReadKey();
+        }
+       
+    }
+} */
